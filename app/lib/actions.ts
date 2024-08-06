4e4a520e880f6e4f93f1af3664e69dd2d03693aa
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-08-06 13:50:51 +0300

    Complete Next js tutorial Section 13

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,15 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
 
+    try {
     await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    `; 
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to create invoice.');
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
@@ -45,21 +50,31 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
+    try {
     await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
     `;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to update invoice.');
+    }
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+    try {
     await sql`
         DELETE FROM invoices
         WHERE id = ${id}
     `;
+    } catch (error) {
+        console.error('Database Error:', error);
+        throw new Error('Failed to delete invoice.');
+    }
 
     revalidatePath("/dashboard/invoices");
 }
